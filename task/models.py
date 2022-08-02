cbe58872814d5b63ec26606a073184737fba56de
Marko Khodan <khodanmarko@gmail.com>
2022-08-02 20:22:03 +0300

    task-list app implemented

--- task/models.py
+++ task/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    done = models.BooleanField()
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["done", "-created"]
